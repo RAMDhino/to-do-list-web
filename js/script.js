0d8e013392bd7363a2dbf8329a767b98f7c20f46
RAMDhino <128151749+RAMDhino@users.noreply.github.com>
2024-03-27 23:38:14 +0700

    refactor: convert program structure
    convert program structure into Object-Oriented Programming while preserving algorithm

--- js/script.js
+++ js/script.js
@@ -1,120 +1,133 @@
-const mainList = document.getElementById("main_list");
-const taskAdder = document.getElementById("task_adder");
-const taskPaper = document.getElementById("task_paper");
+class MainList {
+    constructor(container) {
+        this.container = container;
+        this.taskAdder = document.createElement("div");
+        this.taskAdder.setAttribute("id", "task_adder");
+        this.taskAdder.setAttribute("class", "task-adder");
 
-// Checkbox Prototype
-class Checkbox {
-    constructor(checkboxId) {
-        this.checkboxId = checkboxId;
+        this.taskAdder.addIcon = document.createElement("button");
+        this.taskAdder.addIcon.setAttribute("id", "add_icon");
+        this.taskAdder.addIcon.setAttribute("class", "add-icon");
+
+        this.taskAdder.label = document.createElement("label");
+        this.taskAdder.label.setAttribute("for", "add_icon");
+        this.taskAdder.label.innerHTML = "Click this to add a task!";
+
+
+        this.taskAdder.taskPaper = document.createElement("textarea");
+        this.taskAdder.taskPaper.setAttribute("id", "task_paper");
+        this.taskAdder.taskPaper.setAttribute("class", "task-paper");
+        this.taskAdder.taskPaper.setAttribute("placeholder", "I think I have to ...");
+
+        this.taskAdder.appendChild(this.taskAdder.addIcon);
+        this.taskAdder.appendChild(this.taskAdder.label);
+        this.taskAdder.appendChild(this.taskAdder.taskPaper);
+
+        this.container.appendChild(this.taskAdder);
+
+        this.tasks = [];
+        this.checkboxId = 0;
+
+        this.#addEventListener();
     }
 
-    checkbox = document.createElement("input");
+    makeCheckbox(id) {
+        const checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("name", id);
+        checkbox.setAttribute("id", id);
 
-    makeCheckbox() {
-        this.checkbox.setAttribute("type", "checkbox");
-        this.checkbox.setAttribute("name", this.checkboxId);
-        this.checkbox.setAttribute("id", this.checkboxId);
-
-        this.checkbox.addEventListener("change", () => {
-            if (this.checkbox.checked) {
-                this.checkbox.nextElementSibling.classList.add("stroke-label");
-            } else {
-                this.checkbox.nextElementSibling.classList.remove("stroke-label");
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                checkbox.nextElementSibling.classList.add("stroke-label");
+                return;
             }
+            checkbox.nextElementSibling.classList.remove("stroke-label");
+        });
+        return checkbox;
+    }
+
+    makeLabel(text, id) {
+        const label = document.createElement("label");
+        label.setAttribute("for", id);
+        label.innerHTML = text.trim(); 
+        return label;
+    }
+
+    makeNewTask(labelText, id) {
+        let newTask = document.createElement("div");
+        newTask.setAttribute("class", "task");
+        this.container.insertBefore(newTask, this.taskAdder);
+
+        const checkbox = this.makeCheckbox(id);
+        const label = this.makeLabel(labelText, id);
+
+        const removeIcon = document.createElement("div");
+        removeIcon.setAttribute("class", "remove-icon");
+        removeIcon.style.visibility = "hidden";
+        removeIcon.addEventListener("click", () => {
+            this.container.removeChild(newTask);
+            this.checkTotalTasks();
+        });
+
+        newTask.appendChild(checkbox); 
+        newTask.appendChild(label);
+        newTask.appendChild(removeIcon);
+
+        newTask.addEventListener("mouseout", () => {
+            removeIcon.style.visibility = "hidden";
+        });
+
+        newTask.addEventListener("mouseover", () => {
+            removeIcon.style.visibility = "visible";
+        });
+        this.tasks.push(label.innerHTML);
+    }
+
+    checkTotalTasks() {
+        if (this.container.children.length > 1) {
+            this.taskAdder.children[1].innerHTML = "Adding a Task ...";
+        } else {
+            this.taskAdder.children[1].innerHTML = "Click this to add a task!";
+        }
+    }
+
+    #addEventListener() {
+        this.taskAdder.addEventListener("click", () => {
+            this.taskAdder.children[1].style.display = "none";
+            this.taskAdder.taskPaper.style.display = "inline-block";
+            this.taskAdder.taskPaper.focus();
+        });
+        
+        this.taskAdder.taskPaper.addEventListener("blur", () => {
+            this.taskAdder.children[1].style.display = "inline-block";
+            this.taskAdder.taskPaper.style.display = "none";
+            this.checkTotalTasks();
+        });
+        
+        this.taskAdder.taskPaper.addEventListener("keydown", event => {
+            if (event.key == "Enter") {
+                event.preventDefault();
+                if (this.taskAdder.taskPaper.value !== "") {
+                    this.makeNewTask(this.taskAdder.taskPaper.value, this.checkboxId);
+                    this.taskAdder.taskPaper.value = "";
+                    this.checkboxId++;
+                }
+            }
+        });
+        
+        this.taskAdder.taskPaper.addEventListener("input", event => {
+            let inputValue = event.target.value;
+            if (event.data && event.data.includes("\n")) {
+                inputValue = event.target.value.replace(/\n/g, "");
+                if (this.taskAdder.taskPaper.value !== "") {
+                    this.makeNewTask(this.taskAdder.taskPaper.value, this.checkboxId);
+                    this.taskAdder.taskPaper.value = "";
+                    this.checkboxId++;    
+                }
+             }
         });
     }
 }
 
-// Label Maker
-let makeNewLabel = (text, checkboxID) => {
-    let label = document.createElement("label");
-    label.setAttribute("for", checkboxID);
-    label.innerHTML = text.trim();
-    return label;
-}
-
-
-let checkTotalTasks = () => {
-    if (mainList.children.length > 1) {
-        taskAdder.children[1].innerHTML = "Adding a Task ...";
-    } else {
-        taskAdder.children[1].innerHTML = "Click this to add a task!";
-    }
-}
-
-let IDCounter = 1;
-
-let makeNewTask = taskText => {
-    // New Task
-    let newTask = document.createElement("div");
-    newTask.setAttribute("class", "task");
-    mainList.insertBefore(newTask, taskAdder);
-
-    // Checkbox ID
-    let checkboxID = `list-item-${IDCounter}`;
-    IDCounter++;
-
-    // Checkbox
-    let newTask_Checkbox = new Checkbox(checkboxID);
-    newTask_Checkbox.makeCheckbox();
-
-    // Label Text
-    let newTask_Text = makeNewLabel(taskText, checkboxID);
-
-    // Trash / Delete Icon
-    let removeIcon = document.createElement("div");
-    removeIcon.setAttribute("class", "remove-icon");
-    removeIcon.style.display = "none";
-
-    removeIcon.addEventListener("click", () => {
-        mainList.removeChild(newTask);
-        checkTotalTasks();
-    });
-
-    
-    newTask.appendChild(newTask_Checkbox.checkbox); 
-    newTask.appendChild(newTask_Text);
-    newTask.appendChild(removeIcon);
-
-    newTask.addEventListener("mouseout", () => {
-        removeIcon.style.display = "none";
-    });
-
-    newTask.addEventListener("mouseover", () => {
-        removeIcon.style.display = "block";
-    });
-}
-
-taskAdder.addEventListener("click", () => {
-    taskAdder.children[1].style.display = "none";
-    taskPaper.style.display = "inline-block";
-    taskPaper.focus();
-});
-
-taskPaper.addEventListener("blur", () => {
-    taskAdder.children[1].style.display = "inline-block";
-    taskPaper.style.display = "none";
-
-    checkTotalTasks();
-});
-
-taskPaper.addEventListener("keydown", (event) => {
-    if (event.key == "Enter") {
-        event.preventDefault();
-        if (taskPaper.value !== "") {
-            makeNewTask(taskPaper.value);
-            taskPaper.value = "";    
-        }
-    }
-});
-
-taskPaper.addEventListener("input", (event) => {
-    let inputValue = event.target.value;
-    if (event.data && event.data.includes("\n")) {
-        inputValue = event.target.value.replace(/\n/g, "");
-        if (taskPaper.value !== "") {
-            makeNewTask(taskPaper.value);
-            taskPaper.value = "";    
-        }
-     }
-});
+const mainList = new MainList(document.getElementById("main_list"));
